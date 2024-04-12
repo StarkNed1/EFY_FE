0c1a155d7c55b89b38a2dcd82ef68fe3b9b47689
YangTaeWook <didxodnr0769@gmail.com>
2024-04-12 17:36:54 +0900

    [양태욱][Feat] 화면 이동 기능 추가

---
+++ src/page/form/FormPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const FormPage: React.FC = () => {
+  return (
+    <div>
+      <h1>신청서 양식 페이지입니다.</h1>
+    </div>
+  );
+};
+
+export default FormPage;
---
+++ src/page/gift/GiftPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const GiftPage: React.FC = () => {
+  return (
+    <div>
+      <h1>선물 페이지입니다.</h1>
+    </div>
+  );
+};
+
+export default GiftPage;
